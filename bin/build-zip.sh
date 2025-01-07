b9cc796f651d38fe6227506c3dba6da68af8ee0f
Brandon Fowler <brandfowl@gmail.com>
2025-01-07 15:15:28 -0500

    Backup old vendor folder for build-zip script

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -4,9 +4,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TOP_DIR=$( dirname -- SCRIPT_DIR )
 VERSION=$( grep -oP " \* Version: *\K.+" $TOP_DIR/email-auth.php )
 
+mv vendor vendor-backup
 composer install --no-dev
 
 cd $TOP_DIR
 zip -r email-auth.$VERSION.zip admin includes vendor LICENSE readme.txt email-auth.php uninstall.php
 
-composer install
+rm -r vendor
+mv vendor-backup vendor
