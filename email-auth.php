05eec8e3b2b38c18dd337f602cc348e7d8baa945
Brandon Fowler <brandfowl@gmail.com>
2025-01-15 22:46:35 -0500

    Link to my wordpress plugin landing page

--- email-auth.php
+++ email-auth.php
@@ -15,7 +15,7 @@
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Brandon Fowler
- * Author URI:        https://www.brandonfowler.me/
+ * Author URI:        https://www.brandonfowler.me/wordpress-plugins/
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
